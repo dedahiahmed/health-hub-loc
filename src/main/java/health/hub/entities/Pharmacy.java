ce2bc44b618c2fb8d2d9acd681176665f24ec2d9
dedahiahmed <dedahiahmed10@gmail.com>
2024-05-14 08:59:20 +0000

    add hibernate spatial dependency and pharamcy entity and repos

--- src/main/java/health/hub/entities/Pharmacy.java
+++ src/main/java/health/hub/entities/Pharmacy.java
@@ -1,2 +1,28 @@
-package health.hub.entities;public class Pharmacy {
+package health.hub.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.locationtech.jts.geom.Point;
+
+
+@Entity
+@Table(name = "pharmacy")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Pharmacy {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+  
+    private Point location;
+
+    @Column(name = "is_open_tonight")
+    private boolean isOpenTonight;
 }
--- src/main/java/health/hub/repositories/PharmacyRepository.java
+++ src/main/java/health/hub/repositories/PharmacyRepository.java
@@ -1,2 +1,20 @@
-package health.hub.repositories;public class PharmacyRepository {
+package health.hub.repositories;
+
+import health.hub.entities.Pharmacy;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import jakarta.persistence.TypedQuery;
+
+import java.util.List;
+
+public class PharmacyRepository {
+    EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("myUnit");
+    EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+    public List<Pharmacy> getAll() {
+        String sql = "select p from Pharmacy p ";
+        TypedQuery<Pharmacy> qr = entityManager.createQuery(sql, Pharmacy.class);
+        return qr.getResultList();
+    }
 }
