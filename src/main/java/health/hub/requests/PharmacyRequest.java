a30f7c1d0f99c641118d4b083da4f0e49aa058c6
dedahiahmed <dedahiahmed10@gmail.com>
2024-05-17 20:51:28 -0400

    add precondition

--- src/main/java/health/hub/requests/PharmacyRequest.java
+++ src/main/java/health/hub/requests/PharmacyRequest.java
@@ -1,6 +1,7 @@
 package health.hub.requests;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @Getter
@@ -11,7 +12,9 @@ import lombok.*;
 public class PharmacyRequest {
     @NotBlank(message = "name cannot be null or empty")
     private String name;
+    @NotNull(message = "longitude cannot be null")
     private double longitude;
+    @NotNull(message = "latitude cannot be null")
     private double latitude;
     private boolean isOpenTonight;
 }
