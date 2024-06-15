8fe5e2a3e429f091fd2fc50d2412149cb7b2fee8
dedahiahmed <dedahiahmed10@gmail.com>
2024-06-15 20:10:41 +0000

    add data in sql

--- health.sql
+++ health.sql
@@ -1,8 +1,24 @@
-CREATE TABLE _user
-(
-    id         SERIAL PRIMARY KEY,
-    fullName   VARCHAR(255)        NOT NULL,
-    username   VARCHAR(255) UNIQUE NOT NULL,
-    password   VARCHAR(255)        NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+INSERT INTO public.cabinet (nom, longitude, latitude, willaya, moughataa)
+VALUES ('Cabinet Nouakchott', -15.965985, 18.093242, 'NOUAKCHOTT_NORD', 'TOUJOUNINE'),
+       ('Cabinet El Mina', -15.980734, 18.081596, 'NOUAKCHOTT_SUD', 'ARAFAT'),
+       ('Cabinet Sahara', -15.978512, 18.093045, 'NOUAKCHOTT_OUEST', 'KSAR'),
+       ('Cabinet Al Amal', -15.986672, 18.093973, 'NOUAKCHOTT_SUD', 'DAR_NAIM'),
+       ('Cabinet Al Hayat', -15.983892, 18.096518, 'NOUAKCHOTT_NORD', 'TEVRAGH_ZEINA'),
+       ('Cabinet Al Andalus', -15.989745, 18.098321, 'NOUAKCHOTT_OUEST', 'TEYARET'),
+       ('Cabinet Al Rahma', -15.990574, 18.094256, 'NOUAKCHOTT_NORD', 'RIYAD'),
+       ('Cabinet Al Falah', -15.991761, 18.097479, 'NOUAKCHOTT_OUEST', 'SEBKHA'),
+       ('Cabinet Al Hikma', -15.993224, 18.094865, 'NOUAKCHOTT_SUD', 'EL_MINA'),
+       ('Cabinet Al Madina', -15.996182, 18.091469, 'NOUAKCHOTT_OUEST', 'ARAFAT');
+
+
+INSERT INTO pharmacy (name, longitude, latitude, willaya, moughataa, is_open_tonight)
+VALUES ('Pharmacie Nouakchott', -15.965985, 18.093242, 'NOUAKCHOTT_NORD', 'TOUJOUNINE', FALSE),
+       ('Pharmacie El Mina', -15.980734, 18.081596, 'NOUAKCHOTT_SUD', 'ARAFAT', FALSE),
+       ('Pharmacie Sahara', -15.978512, 18.093045, 'NOUAKCHOTT_OUEST', 'KSAR', FALSE),
+       ('Pharmacie Al Amal', -15.986672, 18.093973, 'NOUAKCHOTT_SUD', 'DAR_NAIM', FALSE),
+       ('Pharmacie Al Hayat', -15.983892, 18.096518, 'NOUAKCHOTT_NORD', 'TEVRAGH_ZEINA', FALSE),
+       ('Pharmacie Al Andalus', -15.989745, 18.098321, 'NOUAKCHOTT_OUEST', 'TEYARET', FALSE),
+       ('Pharmacie Al Rahma', -15.990574, 18.094256, 'NOUAKCHOTT_NORD', 'RIYAD', FALSE),
+       ('Pharmacie Al Falah', -15.991761, 18.097479, 'NOUAKCHOTT_OUEST', 'SEBKHA', FALSE),
+       ('Pharmacie Al Hikma', -15.993224, 18.094865, 'NOUAKCHOTT_SUD', 'EL_MINA', FALSE),
+       ('Pharmacie Al Madina', -15.996182, 18.091469, 'NOUAKCHOTT_OUEST', 'ARAFAT', FALSE);
